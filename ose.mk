f8ee33da773b8ab98bbbbb4da6a4c024c100df74
zooyork0721 <zooyork0721@gmail.com>
2014-01-18 00:23:58 -0500

    Change to OSE

--- ose.mk
+++ ose.mk
@@ -5,18 +5,18 @@ PRODUCT_RELEASE_NAME := d800
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit some common OSE stuff.
+$(call inherit-product, vendor/ose/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ose/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d800/full_d800.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d800
-PRODUCT_NAME := slim_d800
+PRODUCT_NAME := ose_d800
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D800
 PRODUCT_MANUFACTURER := lge
